1d582c3566692477a0ac8de13e474b6f870d9c6d
Edson-Silva-22 <eds.17sv@gmail.com>
2024-07-16 18:04:11 -0300

    começando tela de início

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { Text, View } from "react-native";
-import { Button, Dialog } from '@rneui/themed'
+import { StatusBar, Text, TouchableOpacity, View, SafeAreaView, ActivityIndicator } from "react-native";
+import { Button, Dialog, Header, Icon, Image } from '@rneui/themed'
 import styles from "@/styles/home";
 import { useState } from "react";
 
@@ -7,30 +7,99 @@ export default function Home() {
 const [openModal, setOpenModal] = useState<boolean>(false)
 
     return (
-        <View>
-            <Button
-                title="Open Modal"
-                buttonStyle={styles.button}
-                onPress={() => setOpenModal(true)}
-                type="solid"
-                icon={{
-                    name: 'home',
-                    type: 'font-awesome',
-                    size: 24,
-                    color: 'white',
-                }}           
-            ></Button>
+        <SafeAreaView style={styles.view}>
+            <StatusBar
+                hidden
+            ></StatusBar>
+            <Header
+                containerStyle={styles.header}
+                leftComponent={
+                    <View style={styles.headerLeft}>
+                        <TouchableOpacity>
+                            <Icon name="menu" color="#ccd0cf" />
+                        </TouchableOpacity>
+                    </View>
+                }
+            
+                rightComponent={
+                    <View style={styles.headerRight}>
+                        <TouchableOpacity onPress={() => setOpenModal(true)}>
+                            <Icon 
+                                type="ionicon" 
+                                name="person" 
+                                color="#ccd0cf"
+                            />
+                        </TouchableOpacity>
+
+                        <TouchableOpacity>
+                            <Icon 
+                                type="ionicon" 
+                                name="settings" 
+                                color="#ccd0cf"
+                                size={24}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                }
+            ></Header>
+
+            <View style={styles.main}>
+
+                <Button
+                    title="Open Modal"
+                    buttonStyle={styles.button}
+                    titleStyle={{
+                        color: '#1f2240',
+                    }}
+                    onPress={() => setOpenModal(true)}
+                    type="solid"
+                    icon={{
+                        name: 'home',
+                        type: 'font-awesome',
+                        size: 24,
+                        color: '#1f2240',
+                    }}           
+                ></Button>
+            </View>
+
 
             <Dialog
                 isVisible={openModal}
                 onBackdropPress={() => setOpenModal(false)}
+                overlayStyle={styles.dialog}
             >
-                <Dialog.Title>Dialog Title</Dialog.Title>
-                <Text>Componente Dialog</Text>
+                <Dialog.Title 
+                    title="Meu Perfil" 
+                    titleStyle={{
+                        color: '#ccd0cf',
+                        fontSize: 32,
+                    }}
+                />
+
+                <Image
+                    source={{uri: '../assets/img/pexels-justin-shaifer-501272-1222271.jpg'}}
+                    containerStyle={styles.img}
+                />
+
+                <Text style={styles.label}>Nome</Text>
+
+                <Text style={styles.text}>Alex Silva</Text>
+
+                <Text style={styles.label}>Email</Text>
+
+                <Text style={styles.text}>alex@gmail.com</Text>
+
                 <Dialog.Actions> 
-                    <Button onPress={() => setOpenModal(false)} title="Cancelar" />
+                    <Button 
+                        onPress={() => setOpenModal(false)} 
+                        title="Cancelar" 
+                        buttonStyle={[styles.button, {width: 'auto'}]}
+                        titleStyle={{
+                            color: '#1f2240',
+                        }}
+                    />
                 </Dialog.Actions>
             </Dialog>
-        </View>
+        </SafeAreaView>
     )
 }
--- styles/home.ts
+++ styles/home.ts
@@ -1,11 +1,70 @@
 import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#1f2240",
+  },
+
+  header: {
+    height: 56,
+    width: '100%',
+    backgroundColor: '#1b1c30',
+    borderBottomWidth: 0  
+  },
+
+  headerRight: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 56,
+    gap: 5,
+  },
+
+  headerLeft:{
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 56,
+    gap: 5,
+  },
+
+  main: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: 'center',
+    backgroundColor: "#1f2240",
+  },
+
   button: {
     width: 200,
     height: 50,
     borderRadius: 5,
-  }
+    backgroundColor: '#3dc478',
+  },
+
+  dialog: {
+    backgroundColor: '#1f2240',
+  },
+
+  img: {
+    aspectRatio: 1,
+    width: '100%',
+    flex: 1,
+    borderRadius: 500,
+  },
+
+  label: {
+    fontSize: 16,
+    color: '#4a5c6a'
+  },
+
+  text: {
+    color: '#ccd0cf',
+    fontSize: 18,
+    marginBottom: 20,
+  },
 })
 
 export default styles
